70d54ef66a2d39abda862053e2fc7c2d3fdc64fa
JARVIS . <faisal.farouk-muazu@pau.edu.ng>
2025-10-15 13:30:10 +0100

    (week 3 commit)

---
+++ week-3/practice_2/target/debug/deps/practice_2.d
@@ -0,0 +1,5 @@
+C:\Users\chuch\Documents\f.farouk-muazuCOS101\week-3\practice_2\target\debug\deps\practice_2.d: src\main.rs
+
+C:\Users\chuch\Documents\f.farouk-muazuCOS101\week-3\practice_2\target\debug\deps\practice_2.exe: src\main.rs
+
+src\main.rs:

4e568ff866a8fba8b2d1fd428836674b90f79cac
JARVIS . <faisal.farouk-muazu@pau.edu.ng>
2025-10-15 17:32:32 +0100

    (week 3 practice 6-10)

---
+++ week-3/practice_10/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+C:\Users\chuch\Documents\f.farouk-muazuCOS101\week-3\practice_10\target\debug\practice_10.exe: C:\Users\chuch\Documents\f.farouk-muazuCOS101\week-3\practice_10\src\main.rs
---
+++ week-3/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\chuch\Documents\f.farouk-muazuCOS101\week-3\practice_8\target\debug\practice_8.exe: C:\Users\chuch\Documents\f.farouk-muazuCOS101\week-3\practice_8\src\main.rs
